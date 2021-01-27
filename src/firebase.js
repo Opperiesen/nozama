b92eda59901edd5829c9a9b98c87b1c86f32bd62
Opperiesen <77763298+Opperiesen@users.noreply.github.com>
2021-01-27 01:07:03 +0100

    create-react-app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAsOfS3PCMcvUcffBhoyzpFn3aOcqXHr-w",
+  authDomain: "nozama-e758b.firebaseapp.com",
+  projectId: "nozama-e758b",
+  storageBucket: "nozama-e758b.appspot.com",
+  messagingSenderId: "219433745808",
+  appId: "1:219433745808:web:7a96c210b3d4c4b7d9c715",
+  measurementId: "G-LSJTB96THM",
+};
